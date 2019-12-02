1306ae8b2419e72a1446ffe8a743b00807774294
Carl Drougge <bearded@longhaired.org>
2019-12-03 00:11:09 +0100

    2.2 works.
    It's very slow after finding the answer, presumably because the
    m function has such variable run time. For other inputs it may
    be slow before finding the answer.

--- 2.2.f
+++ 2.2.f
@@ -1,15 +1,25 @@
 ß
-[0$[^$$47>\57>~&][48-\10*+\1+\]#%\0=[%1_]?]i:
-[p;\-;4*a;\-;]r:
-[$2ø>[\]?0\[$0>][1-\2ø+\]#%\%]m:
+[0$[^$$47>\57>~&][48-\10*+\1+\]#%\0=[%1_]?]i: { input integer           }
+[p;\-;4*a;\-;]r:                              { read pc-relative        }
+[$2ø>[\]?0\[$0>][1-\2ø+\]#%\%]m:              { 32bit mul               }
+[l;4*[$0>][$$o;\-;\a;\-:4-]#%]g:              { init (copy from o to a) }
 
 0[i;!$1_=~][\1+]#% { program in reverse, len }
 
-4*11021`+a: { top of reversed program in a  (11021` is MOVE.L A5,-(A5)) }
+{
+	11021` is MOVE.L A5,-(A5)
+	a and o both get their own copy of the (reversed) program.
+}
+$4*4+11021`+o:
+$l:1+[$0>][l;1+ø\1-]#%l;
+4*11021`+a:
 
-a;p:
-12p;4-: 2p;8-:
-
+0n:[n;99>~][
+0v:[v;99>~][
+a;p:g;!n;p;4-:v;p;8-:
 [p;;$99=~][1=4r;!8r;!@$[%+1_]?~m;?p;12-;4*a;\-:p;16-p:]#
 
-a;;.10,
+246134 80m;! { this is 19690720 but the FALSE parser can't parse that. }
+a;;=[n;100*v;+.10,]?
+v;1+v:]#
+n;1+n:]#
